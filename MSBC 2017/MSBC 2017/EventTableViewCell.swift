ca141bf87ff8c481766bfff85f02a4da730bf309
Nathan Gupta <Nathan-MAC@Nathans-MacBook-Pro-2.local>
2017-07-30 02:55:15 -0700

    Initial Commit. Working on Event Table Cell View

---
+++ MSBC 2017/MSBC 2017/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  MSBC 2017
+//
+//  Created by Nathan Gupta on 7/30/17.
+//  Copyright © 2017 MSBC. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MSBC 2017/MSBC 2017Tests/MSBC_2017Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MSBC_2017Tests.swift
+//  MSBC 2017Tests
+//
+//  Created by Nathan Gupta on 7/30/17.
+//  Copyright © 2017 MSBC. All rights reserved.
+//
+
+import XCTest
+@testable import MSBC_2017
+
+class MSBC_2017Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
